545fcf4f714dea4cbb086f2001c9949e8dd09393
Erick Jimenez <erick.jimenezr04@gmail.com>
2017-05-26 01:48:07 -0400

    close issue #4 related with background music

--- app/src/main/java/com/example/erick/connect4/MainActivity.java
+++ app/src/main/java/com/example/erick/connect4/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.erick.connect4;
 
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int dashboard[][] = new int[7][6];
     private int player1;
     private int player2;
+    private MediaPlayer mp;
 
     private byte playerTurn;
 
@@ -30,11 +32,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.col6).setOnClickListener(this);
         findViewById(R.id.col7).setOnClickListener(this);
 
+        mp = MediaPlayer.create(this, R.raw.bg);
+        mp.setVolume(100, 100);
+        mp.start();
+
         player1 = getResources()
                 .getIdentifier("red", "drawable", getPackageName());
         player2 = getResources()
                 .getIdentifier("yellow", "drawable", getPackageName());
         playerTurn = 1;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mp.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mp.start();
     }
 
     @Override
